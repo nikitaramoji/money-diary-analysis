e0abde871a2aeaf5e43678ba7ba9c38ca164c632
nikitaramoji <nikita_ramoji@brown.edu>
2020-05-22 17:34:27 -0700

    Cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # money-diary-analysis
+
+This is a fun side project to play with the data from Refinery29 Money Diaries.
+For now, you can run the money-diaries-generator.py file in order to create
+a generated money diary in refinery29-generated-money-diary.txt.
--- money-diaries-generator.py
+++ money-diaries-generator.py
@@ -2,20 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import markov_novel
 import markovify
+import pickle
 from pprint import pprint
 
 general_url = 'https://www.refinery29.com/en-us/money-diary?page='
+corpus_url = 'corpus.txt'
 all_urls = []
 
-def get_all_urls(general_url, all_urls, n):
-    for i in range(1, n+1):
+def get_all_urls(general_url):
+    print("Getting all URLs")
+    continue_boolean = True
+    i = 1
+    while(continue_boolean):
+        print("On page " + str(i))
         page_url = general_url + str(i)
         response = requests.get(page_url)
         soup = BeautifulSoup(response.text, 'html.parser')
         page_divs = soup.find_all('div', ['card prime', 'card featured-story', 'card standard'])
-        for div in page_divs:
-            if '/en-us/money-diary-' == div.a.get('href')[0:19]:
-                all_urls.append('https://www.refinery29.com/' + div.a.get('href'))
+        if not page_divs:
+            continue_boolean = False
+        else:
+            i += 1
+            for div in page_divs:
+                if '/en-us/money-diary-' == div.a.get('href')[0:19]:
+                    all_urls.append('https://www.refinery29.com/' + div.a.get('href'))
+    return all_urls
 
 def get_single_url(url):
     response = requests.get(url)
@@ -26,16 +37,31 @@ def get_single_url(url):
         page_text += item.get_text()
     return page_text
 
+def generate_corpus(general_url):
+    all_urls = get_all_urls(general_url)
+    corpus = []
+    for url in all_urls:
+        corpus.append(get_single_url(url))
+    f = open('corpus.txt', 'wb')
+    pickle.dump(corpus, f)
+    f.close()
+
+def generate_markovify_money_diary(corpus_url):
+    f = open(corpus_url, 'rb')
+    text = f.read().decode("utf-8")
+    f.close()
+    print("Generating text model")
+    text_model = markovify.Text(text)
+    print("Generating Markov Novel")
+    novel = markov_novel.Novel(text_model, chapter_count=3)
+    print("Writing to file")
+    novel.write(novel_title='index', filetype='md')
+    print("Done!")
+
 
 def main():
-  # get_all_urls(general_url, all_urls, 31)
-  corpus = ''
-  get_all_urls(general_url, all_urls, 3)
-  for url in all_urls:
-    corpus += get_single_url(url)
-  text_model = markovify.Text(corpus)
-  novel = markov_novel.Novel(text_model, chapter_count=20)
-  novel.write(novel_title='refinery29-generated-money-diary', filetype='txt')
+  # generate_corpus(general_url)
+  generate_markovify_money_diary(corpus_url)
 
 if __name__== "__main__":
   main()
